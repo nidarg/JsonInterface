e2b7c0e938e5ab1c8a4847dccea06b1297b52d9a
Gradin <gradin_g@yahoo.com>
2019-05-27 12:44:42 +0300

    test choice success and choice remaining text

---
+++ JsonInterface/JsonInterface/Choice/Choices.cs
@@ -0,0 +1,38 @@
+﻿using JsonInterface.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonInterface.Choice
+{
+    public class Choices : IPattern
+    {
+        IPattern[] patterns;
+
+        public Choices(params IPattern[] patterns)
+        {
+            this.patterns = patterns;
+        }
+
+        public IMatch Match(string text)
+        {
+            string lastMatch = text;
+            int i = 0;
+            foreach (var pattern in patterns)
+            {
+
+                if (pattern.Match(text).Success() == true)
+                {
+                    //text = pattern.Match(text).RemainingText();
+                    text = text.Substring(1);
+
+                    //pattern.Match(text);
+                    i++;
+                }
+                else return pattern.Match(text);
+            }
+
+            return patterns[i - 1].Match(lastMatch.Substring(i - 1));
+        }
+    }
+}
---
+++ JsonInterfaceValidation/ChoiceTest/ChoiceTest.cs
@@ -0,0 +1,41 @@
+﻿using JsonInterface.Character;
+using JsonInterface.Choice;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace JsonInterfaceValidation.ChoiceTest
+{
+    public class ChoiceTest
+    {
+        [Theory]
+        [InlineData('1', '1', '9', true)]
+        public void TestChoiceSuccess(char c1, char c2, char c3, bool expected)
+        {
+            var digit = new Choices(
+                new Charact(c1),
+                new Charact(c2),
+               new Charact(c3)
+
+                );
+            bool actual = digit.Match("11945").Success();
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData('1', '2', '9', "45")]
+        public void TestChoiceRemainingText(char c1, char c2, char c3, string expected)
+        {
+            var digit = new Choices(
+               new Charact(c1),
+               new Charact(c2),
+               new Charact(c3)
+
+                );
+
+            string actual = digit.Match("12945").RemainingText();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
