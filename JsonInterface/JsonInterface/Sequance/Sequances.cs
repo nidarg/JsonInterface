eaf485f820e4aa5ba5caf06ef03024b1e5265566
Gradin <gradin_g@yahoo.com>
2019-05-27 12:51:16 +0300

    test sequance methods

---
+++ JsonInterface/JsonInterface/Sequance/Sequances.cs
@@ -0,0 +1,47 @@
+﻿using JsonInterface.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonInterface.Sequance
+{
+    public class Sequances : IPattern
+    {
+        IPattern[] patterns;
+
+
+        public Sequances(params IPattern[] patterns)
+        {
+            this.patterns = patterns;
+        }
+
+        public IMatch Match(string text)
+        {
+            string lastMatch = text;
+            int i = 0; int j = 0;
+            foreach (var pattern in patterns)
+            {
+               
+                if (pattern.Match(text).Success() == true)
+                {
+                    if (pattern is Sequances)
+                    {
+                        text = text.Substring(1);
+                        //i=1;
+                        j++;
+                    }
+                    text = text.Substring(1);
+
+                    Console.WriteLine("text after success " + text);
+                    i++;
+                    j++;
+                }
+                else return pattern.Match(text);
+            }
+            
+            return patterns[i - 1].Match(lastMatch.Substring(j - 1));
+        }
+
+
+    }
+}
---
+++ JsonInterfaceValidation/SequanceTest/SequanceTest.cs
@@ -0,0 +1,52 @@
+﻿using JsonInterface.Character;
+using JsonInterface.Sequance;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace JsonInterfaceValidation.SequanceTest
+{
+    public class SequanceTest
+    {
+        [Theory]
+        [InlineData('a', 'b', 'c', true)]
+        public void TestSequanceSuccess(char c1, char c2, char c3, bool expected)
+        {
+            var ab = new Sequances(
+                new Charact(c1),
+                new Charact(c2)
+                );
+
+            var abc = new Sequances(
+                    ab,
+                    new Charact(c3)
+                    );
+
+
+            bool actual = abc.Match("abcd").Success();
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData('a', 'b', 'c', "d")]
+        public void TestSequanceRemainingText(char c1, char c2, char c3, string expected)
+        {
+            var ab = new Sequances(
+                new Charact(c1),
+                new Charact(c2)
+                );
+
+            var abc = new Sequances(
+                    ab,
+                    new Charact(c3)
+                    );
+
+
+            string actual = abc.Match("abcd").RemainingText();
+            Assert.Equal(expected, actual);
+        }
+
+
+    }
+}
